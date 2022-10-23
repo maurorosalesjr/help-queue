497624544710f7dad3426e82326990f63625ae96
Mauro Rosales Jr <mauro.rosales247@gmail.com>
2022-10-23 10:15:20 -0700

    commit before Redux

---
+++ src/components/ReusableForm.js
@@ -0,0 +1,33 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function ReusableForm(props) {
+  return (
+    <React.Fragment>
+      <form onSubmit={props.formSubmissionHandler}>
+        <input
+          type='text'
+          name='names'
+          placeholder='Pair Names' />
+          <br></br>
+        <input
+          type='text'
+          name='location'
+          placeholder='Location' />
+          <br></br>
+        <textarea
+          name='issue'
+          placeholder='Describe your issue.' />
+          <br></br>
+        <button type='submit'>{props.buttonText}</button>
+      </form>
+    </React.Fragment>
+  );
+}
+
+ReusableForm.propTypes = {
+  formSubmissionHandler: PropTypes.func,
+  buttonText: PropTypes.string
+};
+
+export default ReusableForm;
